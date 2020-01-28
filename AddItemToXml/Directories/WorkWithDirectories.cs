93abe83b4a101019c238c4e5de3b06e4c7ff0451
user <user@DESKTOP-SFHS1DK>
2020-01-28 22:35:50 +0400

    mk

---
+++ AddItemToXml/Directories/WorkWithDirectories.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Directories
+{
+    static class WorkWithDirectories
+    {
+        public static void FirstMethod()
+        {
+            string dirName = "C:\\";
+
+            if (Directory.Exists(dirName))
+            {
+                Console.WriteLine("Подкаталоги:");
+                string[] dirs = Directory.GetDirectories(dirName);
+                foreach (string s in dirs)
+                {
+                    Console.WriteLine(s);
+                }
+                Console.WriteLine();
+                Console.WriteLine("Файлы:");
+                string[] files = Directory.GetFiles(dirName);
+                foreach (string s in files)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+        }
+    }
+}
